c892bc404224e1604ec8dd48245f6df3258c666d
j.cordes <j.cordes@fz-juelich.de>
2021-09-02 10:46:40 +0200

    Updated the Path to the new dir

--- 2D_model_library.jl
+++ 2D_model_library.jl
@@ -1,23 +1,21 @@
-include("/home/jakob/Dokumente/Promotion/Julia_Notebook/Library_new/struct.jl")
-include("/home/jakob/Dokumente/Promotion/Julia_Notebook/Library_new/vectors.jl")
-include("/home/jakob/Dokumente/Promotion/Julia_Notebook/Library_new/initialize_system.jl")
-include("/home/jakob/Dokumente/Promotion/Julia_Notebook/Library_new/neighbors.jl")
-include("/home/jakob/Dokumente/Promotion/Julia_Notebook/Library_new/initialize_geometry.jl")
-include("/home/jakob/Dokumente/Promotion/Julia_Notebook/Library_new/mengen.jl")
-include("/home/jakob/Dokumente/Promotion/Julia_Notebook/Library_new/plot_functions.jl")
+include("./struct.jl")
+include("./vectors.jl")
+include("./initialize_system.jl")
+include("./neighbors.jl")
+include("./initialize_geometry.jl")
+include("./mengen.jl")
+include("./plot_functions.jl")
 
-include("/home/jakob/Dokumente/Promotion/Julia_Notebook/Library_new/observables.jl")
+include("./observables.jl")
 
+include("./distance_based_model_base_functions.jl")
+include("./distance_based_model_direction.jl")
+include("./distance_based_model_velocity.jl")
+include("./distance_based_model_complete.jl")
 
-
-include("/home/jakob/Dokumente/Promotion/Julia_Notebook/Library_new/distance_based_model_base_functions.jl")
-include("/home/jakob/Dokumente/Promotion/Julia_Notebook/Library_new/distance_based_model_direction.jl")
-include("/home/jakob/Dokumente/Promotion/Julia_Notebook/Library_new/distance_based_model_velocity.jl")
-include("/home/jakob/Dokumente/Promotion/Julia_Notebook/Library_new/distance_based_model_complete.jl")
-
-include("/home/jakob/Dokumente/Promotion/Julia_Notebook/Library_new/ttc.jl")
-include("/home/jakob/Dokumente/Promotion/Julia_Notebook/Library_new/ttc_based_model_velocity.jl")
-include("/home/jakob/Dokumente/Promotion/Julia_Notebook/Library_new/ttc_based_model_complete.jl")
+include("./ttc.jl")
+include("./ttc_based_model_velocity.jl")
+include("./ttc_based_model_complete.jl")
 
 function start_simulation(menge, geometrie, sim_p, velocity_model, boundaries, system_size=(0,0))
 
