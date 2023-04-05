30a70860debd5b57f975a183d8a3d51ee4ac64e3
j.cordes <j.cordes@fz-juelich.de>
2023-04-05 15:48:48 +0200

    Update desired headings changed

--- 2D_Models/Update_Functions.jl
+++ 2D_Models/Update_Functions.jl
@@ -17,6 +17,8 @@ function Update_Desired_Headings!(menge::crowd, system_size)
         x.e_des = e_(x, x.goal).*(-1)
 
         x.v_des = clamp(d(x.pos, x.goal, system_size), 0.001, x.v_des)
+        x.v_max = clamp(d(x.pos, x.goal, system_size), 0.001, x.v_max)
+
 
     end
 end
