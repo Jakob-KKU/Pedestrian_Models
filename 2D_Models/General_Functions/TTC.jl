75dc1bc33ba0917fe971d05d2a82b8039dffd2e5
j.cordes <j.cordes@fz-juelich.de>
2023-07-10 09:44:11 +0200

    TTC tidy up, AV model social radius added

--- 2D_Models/General_Functions/TTC.jl
+++ 2D_Models/General_Functions/TTC.jl
@@ -1,5 +1,6 @@
 ### the functions to calculate the ttc are defined ###
 
+#simple ttc between two agents a and b
 function ttc(a::agent, b::agent)
 
     cos_α = e_(a, b)⋅e_v(a, b)
@@ -33,12 +34,117 @@ function ttc(a::agent, b::agent, v_a::Float64, v_b::Float64)
     cos_α = e_(a, b)⋅e_v(a, b, v_a, v_b)
     A = ((cos_α)^2-1)*d(a,b)^2+l(a, b)^2
 
-    if A < 0 || -(cos_α)*d(a,b)-sqrt(A) < 0
-        999
+    if A < 0 || -(cos_α)*d(a,b)-sqrt(A) < 0.0
+        999.9
     else
         (-(cos_α)*d(a,b)-sqrt(A))/abs(Δv(a, b, v_a, v_b))
     end
 end
+
+function ttc(a::agent, b::agent, a_heading::NTuple{2, Float64}, a_vel::Float64)
+
+    cos_α = e_(a,b)⋅e_v(a, b, a_heading, a_vel)
+    A = (cos_α^2-1)*d(a,b)^2+l(a, b)^2
+
+
+    if A < 0 || -cos_α*d(a,b)-sqrt(A) < 0 || abs(Δv(a,b, a_heading, a_vel)) == 0
+        999.9
+    else
+        (-cos_α*d(a,b)-sqrt(A))/abs(Δv(a,b, a_heading, a_vel))
+    end
+end
+
+#simple ttc between two agents a and b in a periodic system
+function ttc(a::agent, b::agent, system_size::NTuple{2, Float64})
+
+    cos_α = e_(a,b,system_size)⋅e_v(a, b)
+    A = ((cos_α)^2-1)*d(a,b,system_size)^2+l(a, b)^2
+
+    if A < 0 || -(cos_α)*d(a,b,system_size)-sqrt(A) < 0
+        999
+    else
+        (-(cos_α)*d(a,b,system_size)-sqrt(A))/abs(Δv(a,b))
+    end
+end
+
+function ttc(a::agent, b::agent, a_heading::NTuple{2, Float64}, system_size::NTuple{2, Float64})
+
+    cos_α = e_(a,b,system_size)⋅e_v(a, b, a_heading)
+    A = (cos_α^2-1)*d(a,b,system_size)^2+l(a, b)^2
+
+
+    if A < 0 || -cos_α*d(a,b,system_size)-sqrt(A) < 0 || abs(Δv(a,b, a_heading)) == 0
+        999.9
+    else
+        (-cos_α*d(a,b,system_size)-sqrt(A))/abs(Δv(a,b, a_heading))
+    end
+end
+
+function ttc(a::agent, b::agent, v_a::Float64, system_size::NTuple{2, Float64})
+
+    cos_α = e_(a, b, system_size)⋅e_v(a, b, v_a)
+    A = ((cos_α)^2-1)*d(a,b,system_size)^2+l(a, b)^2
+
+    if A < 0 || -(cos_α)*d(a,b,system_size)-sqrt(A) < 0
+        999
+    else
+        (-(cos_α)*d(a,b,system_size)-sqrt(A))/abs(Δv(a,b,v_a))
+    end
+end
+
+function ttc(a::agent, b::agent, a_heading::NTuple{2, Float64}, a_vel::Float64, system_size::NTuple{2, Float64})
+
+    cos_α = e_(a,b,system_size)⋅e_v(a, b, a_heading, a_vel)
+    A = (cos_α^2-1)*d(a,b,system_size)^2+l(a, b)^2
+
+
+    if A < 0 || -cos_α*d(a,b,system_size)-sqrt(A) < 0 || abs(Δv(a,b, a_heading, a_vel)) == 0
+        999.9
+    else
+        (-cos_α*d(a,b,system_size)-sqrt(A))/abs(Δv(a,b, a_heading, a_vel))
+    end
+end
+
+function ttc(a::agent, b::agent, a_heading::NTuple{2, Float64}, a_vel::Float64, dia_effectiv, system_size::NTuple{2, Float64})
+
+    cos_α = e_(a,b,system_size)⋅e_v(a, b, a_heading, a_vel)
+    A = (cos_α^2-1)*d(a,b,system_size)^2+dia_effectiv^2
+
+
+    if A < 0 || -cos_α*d(a,b,system_size)-sqrt(A) < 0 || abs(Δv(a,b, a_heading, a_vel)) == 0
+        999.9
+    else
+        (-cos_α*d(a,b,system_size)-sqrt(A))/abs(Δv(a,b, a_heading, a_vel))
+    end
+end
+
+function ttc(a::agent, b::agent, a_heading::NTuple{2, Float64}, v_b_approx::Float64,
+    system_size::NTuple{2, Float64})
+
+    cos_α = e_(a,b,system_size)⋅e_v(a, b, a_heading, v_b_approx)
+    A = (cos_α^2-1)*d(a,b,system_size)^2+l(a, b)^2
+
+    if A < 0 || -cos_α*d(a,b,system_size)-sqrt(A) < 0
+        999.9
+    else
+        (-cos_α*d(a,b,system_size)-sqrt(A))/abs(Δv(a,b,a_heading,v_b_approx))
+    end
+end
+
+function ttc(a::agent, b::agent, v_a::Float64, v_b::Float64,  system_size::NTuple{2, Float64})
+
+    cos_α = e_(a, b, system_size)⋅e_v(a, b, v_a, v_b)
+    A = ((cos_α)^2-1)*d(a,b,system_size)^2+l(a, b)^2
+
+    if A < 0 || -(cos_α)*d(a,b,system_size)-sqrt(A) < 0
+        999
+    else
+        (-(cos_α)*d(a,b,system_size)-sqrt(A))/abs(Δv(a,b,v_a, v_b))
+    end
+end
+
+
+#simple ttc between agent a and geometry element b
 
 function ttc(a::agent, b::element)
 
@@ -65,54 +171,17 @@ function ttc(a::agent, b::element, v_a::Float64)
     end
 end
 
-function ttc(a::agent, b::agent, a_heading::NTuple{2, Float64}, a_vel::Float64)
 
-    cos_α = e_(a,b)⋅e_v(a, b, a_heading, a_vel)
-    A = (cos_α^2-1)*d(a,b)^2+l(a, b)^2
+#simple ttc between agent a and geometry element b in a periodic system
+function ttc(a::agent, b::element, system_size::NTuple{2, Float64})
 
+    cos_α = e_(a, b, system_size)⋅a.heading
+    A = (cos_α^2-1)*d(a,b,system_size)^2+l(a,b)^2
 
-    if A < 0 || -cos_α*d(a,b)-sqrt(A) < 0 || abs(Δv(a,b, a_heading, a_vel)) == 0
-        999.9
+    if  A < 0 || (-cos_α*d(a,b,system_size) - sqrt(A)) < 0
+        return 999
     else
-        (-cos_α*d(a,b)-sqrt(A))/abs(Δv(a,b, a_heading, a_vel))
-    end
-end
-
-function ttc(a::agent, b::agent, system_size::NTuple{2, Float64})
-
-    cos_α = e_(a,b,system_size)⋅e_v(a, b)
-    A = ((cos_α)^2-1)*d(a,b,system_size)^2+l(a, b)^2
-
-    if A < 0 || -(cos_α)*d(a,b,system_size)-sqrt(A) < 0
-        999
-    else
-        (-(cos_α)*d(a,b,system_size)-sqrt(A))/abs(Δv(a,b))
-    end
-end
-
-function ttc(a::agent, b::agent, a_heading::NTuple{2, Float64}, system_size::NTuple{2, Float64})
-
-    cos_α = e_(a,b,system_size)⋅e_v(a, b, a_heading)
-    A = (cos_α^2-1)*d(a,b,system_size)^2+l(a, b)^2
-
-
-    if A < 0 || -cos_α*d(a,b,system_size)-sqrt(A) < 0 || abs(Δv(a,b, a_heading)) == 0
-        999.9
-    else
-        (-cos_α*d(a,b,system_size)-sqrt(A))/abs(Δv(a,b, a_heading))
-    end
-end
-
-function ttc(a::agent, b::agent, a_heading::NTuple{2, Float64}, v_b_approx::Float64,
-    system_size::NTuple{2, Float64})
-
-    cos_α = e_(a,b,system_size)⋅e_v(a, b, a_heading, v_b_approx)
-    A = (cos_α^2-1)*d(a,b,system_size)^2+l(a, b)^2
-
-    if A < 0 || -cos_α*d(a,b,system_size)-sqrt(A) < 0
-        999.9
-    else
-        (-cos_α*d(a,b,system_size)-sqrt(A))/abs(Δv(a,b,a_heading,v_b_approx))
+        return (-cos_α*d(a,b,system_size) - sqrt(A))/a.vel
     end
 end
 
@@ -129,52 +198,15 @@ function ttc(a::agent, b::element, a_heading::NTuple{2, Float64}, system_size::N
     end
 end
 
-function ttc(a::agent, b::agent, v_a::Float64, system_size::NTuple{2, Float64})
+function ttc(a::agent, b::element, v_a::Float64, system_size::NTuple{2, Float64})
 
-    cos_α = e_(a, b, system_size)⋅e_v(a, b, v_a)
-    A = ((cos_α)^2-1)*d(a,b,system_size)^2+l(a, b)^2
-
-    if A < 0 || -(cos_α)*d(a,b,system_size)-sqrt(A) < 0
-        999
-    else
-        (-(cos_α)*d(a,b,system_size)-sqrt(A))/abs(Δv(a,b,v_a))
-    end
-end
-
-function ttc(a::agent, b::agent, v_a::Float64, v_b::Float64,  system_size::NTuple{2, Float64})
-
-    cos_α = e_(a, b, system_size)⋅e_v(a, b, v_a, v_b)
-    A = ((cos_α)^2-1)*d(a,b,system_size)^2+l(a, b)^2
-
-    if A < 0 || -(cos_α)*d(a,b,system_size)-sqrt(A) < 0
-        999
-    else
-        (-(cos_α)*d(a,b,system_size)-sqrt(A))/abs(Δv(a,b,v_a, v_b))
-    end
-end
-
-function ttc(a::agent, b::element, system_size::NTuple{2, Float64})
-
-    cos_α = e_(a, b, system_size)⋅a.heading
+    cos_α = e_(a, b ,system_size)⋅a.heading
     A = (cos_α^2-1)*d(a,b,system_size)^2+l(a,b)^2
 
     if  A < 0 || (-cos_α*d(a,b,system_size) - sqrt(A)) < 0
         return 999
     else
-        return (-cos_α*d(a,b,system_size) - sqrt(A))/a.vel
-    end
-end
-
-function ttc(a::agent, b::agent, a_heading::NTuple{2, Float64}, a_vel::Float64, system_size::NTuple{2, Float64})
-
-    cos_α = e_(a,b,system_size)⋅e_v(a, b, a_heading, a_vel)
-    A = (cos_α^2-1)*d(a,b,system_size)^2+l(a, b)^2
-
-
-    if A < 0 || -cos_α*d(a,b,system_size)-sqrt(A) < 0 || abs(Δv(a,b, a_heading, a_vel)) == 0
-        999.9
-    else
-        (-cos_α*d(a,b,system_size)-sqrt(A))/abs(Δv(a,b, a_heading, a_vel))
+        return (-cos_α*d(a,b,system_size) - sqrt(A))/v_a
     end
 end
 
@@ -191,25 +223,57 @@ function ttc(a::agent, b::element, a_heading::NTuple{2, Float64}, a_vel::Float64
     end
 end
 
-function ttc(a::agent, b::element, v_a::Float64, system_size::NTuple{2, Float64})
+#calculates the minimal TTC with different Variables
 
-    cos_α = e_(a, b ,system_size)⋅a.heading
-    A = (cos_α^2-1)*d(a,b,system_size)^2+l(a,b)^2
+function Min_TTC(a::agent, menge::crowd, geometrie::geometry, system_size::NTuple{2, Float64})
 
-    if  A < 0 || (-cos_α*d(a,b,system_size) - sqrt(A)) < 0
-        return 999
-    else
-        return (-cos_α*d(a,b,system_size) - sqrt(A))/v_a
-    end
+    ttc_geo = Min_TTC_Geometry(a, geometrie, system_size)
+    ttc_agents = Min_TTC_Agents(a, menge, system_size)
+
+    min(ttc_geo, ttc_agents)
+
 end
 
-#calculates the minimal TTC
 function Min_TTC(a::agent, a_heading::NTuple{2, Float64}, menge::crowd, geometrie::geometry, system_size::NTuple{2, Float64})
 
     ttc_geo = Min_TTC_Geometry(a, a_heading, geometrie, system_size)
     ttc_agents = Min_TTC_Agents(a, a_heading, menge, system_size)
 
     min(ttc_geo, ttc_agents)
+end
+
+function Min_TTC(a::agent, a_vel::Float64, a_heading::NTuple{2, Float64}, menge::crowd, geometrie::geometry,
+        system_size::NTuple{2, Float64})
+
+    ttc_geo = Min_TTC_Geometry(a, a_vel, a_heading, geometrie, system_size)
+    ttc_agents = Min_TTC_Agents(a, a_vel, a_heading, menge, system_size)
+
+    min(ttc_geo, ttc_agents)
+
+end
+
+function Min_TTC(a::agent, a_vel::Float64, a_heading::NTuple{2, Float64}, r_soc::Float64, menge::crowd, geometrie::geometry,
+        system_size::NTuple{2, Float64})
+
+    ttc_geo = Min_TTC_Geometry(a, a_vel, a_heading, geometrie, system_size)
+    ttc_agents = Min_TTC_Agents(a, a_vel, a_heading, r_soc, menge, system_size)
+
+    min(ttc_geo, ttc_agents)
+
+end
+
+#calculates the minimal TTC with the other Agents with different Variables
+
+function Min_TTC_Agents(a::agent, menge::crowd, system_size::NTuple{2, Float64})
+
+    ttc_min = 999.0
+
+    for i in 2:a.neighbors_agents[1]+1
+
+        ttc_help = ttc(a, menge.agent[a.neighbors_agents[i]], system_size)
+        ttc_min = min(ttc_help, ttc_min)
+    end
+    ttc_min
 end
 
 function Min_TTC_Agents(a::agent, a_heading::NTuple{2, Float64}, menge::crowd, system_size::NTuple{2, Float64})
@@ -226,62 +290,6 @@ function Min_TTC_Agents(a::agent, a_heading::NTuple{2, Float64}, menge::crowd, s
     ttc_min
 end
 
-function Min_TTC_Geometry(a::agent, a_heading::NTuple{2, Float64}, geometrie::geometry, system_size::NTuple{2, Float64})
-
-    ttc_min = 999.0
-
-    for i in 2:a.neighbors_geometry[1]+1
-
-        ttc_help = ttc(a, geometrie.element[a.neighbors_geometry[i]], a_heading, system_size)
-
-        ttc_min = min(ttc_help, ttc_min)
-    end
-    ttc_min
-end
-
-function Min_TTC(a::agent, menge::crowd, geometrie::geometry, system_size::NTuple{2, Float64})
-
-    ttc_geo = Min_TTC_Geometry_measure(a, geometrie, system_size)
-    ttc_agents = Min_TTC_Agents_measure(a, menge, system_size)
-
-    min(ttc_geo, ttc_agents)
-
-end
-
-function Min_TTC_Agents_measure(a::agent, menge::crowd, system_size::NTuple{2, Float64})
-
-    ttc_min = 999.0
-
-    for i in 2:a.neighbors_agents[1]+1
-
-        ttc_help = ttc(a, menge.agent[a.neighbors_agents[i]], system_size)
-        ttc_min = min(ttc_help, ttc_min)
-    end
-    ttc_min
-end
-
-function Min_TTC_Geometry_measure(a::agent, geometrie::geometry, system_size::NTuple{2, Float64})
-
-    ttc_min = 999.0
-
-    for i in 2:a.neighbors_geometry[1]+1
-
-        ttc_help = ttc(a, geometrie.element[a.neighbors_geometry[i]], system_size)
-
-        ttc_min = min(ttc_help, ttc_min)
-    end
-    ttc_min
-end
-function Min_TTC(a::agent, a_vel::Float64, a_heading::NTuple{2, Float64}, menge::crowd, geometrie::geometry,
-        system_size::NTuple{2, Float64})
-
-    ttc_geo = Min_TTC_Geometry(a, a_vel, a_heading, geometrie, system_size)
-    ttc_agents = Min_TTC_Agents(a, a_vel, a_heading, menge, system_size)
-
-    min(ttc_geo, ttc_agents)
-
-end
-
 function Min_TTC_Agents(a::agent, a_vel::Float64, a_heading::NTuple{2, Float64}, menge::crowd,
         system_size::NTuple{2, Float64})
 
@@ -294,6 +302,55 @@ function Min_TTC_Agents(a::agent, a_vel::Float64, a_heading::NTuple{2, Float64},
         ttc_min = min(ttc_help, ttc_min)
     end
 
+    ttc_min
+end
+
+function Min_TTC_Agents(a::agent, a_vel::Float64, a_heading::NTuple{2, Float64}, r_soc, menge::crowd,
+        system_size::NTuple{2, Float64})
+
+    ttc_min = 999.0
+
+    for i in 2:a.neighbors_agents[1]+1
+
+        #the other agent
+        b = menge.agent[a.neighbors_agents[i]]
+
+        #the social diameter
+        dia_effective = 2*min(r_soc, d(a, b)/4)
+
+        ttc_help = ttc(a, b, a_heading, a_vel, dia_effectiv, system_size)
+
+        ttc_min = min(ttc_help, ttc_min)
+    end
+
+    ttc_min
+end
+
+#calculates the minimal TTC with the Geometry with different Variables
+
+function Min_TTC_Geometry(a::agent, geometrie::geometry, system_size::NTuple{2, Float64})
+
+    ttc_min = 999.0
+
+    for i in 2:a.neighbors_geometry[1]+1
+
+        ttc_help = ttc(a, geometrie.element[a.neighbors_geometry[i]], system_size)
+
+        ttc_min = min(ttc_help, ttc_min)
+    end
+    ttc_min
+end
+
+function Min_TTC_Geometry(a::agent, a_heading::NTuple{2, Float64}, geometrie::geometry, system_size::NTuple{2, Float64})
+
+    ttc_min = 999.0
+
+    for i in 2:a.neighbors_geometry[1]+1
+
+        ttc_help = ttc(a, geometrie.element[a.neighbors_geometry[i]], a_heading, system_size)
+
+        ttc_min = min(ttc_help, ttc_min)
+    end
     ttc_min
 end
 
